cae9e94a2da03e2489e3ed3b6ed9e4f26b55461b
Saket Choudhary <saketkc@gmail.com>
2026-03-03 22:46:14 +0530

    Add footer

---
+++ components/footer.tsx
@@ -0,0 +1,18 @@
+import { Flex, Link, Separator, Text } from "@radix-ui/themes";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Separator size="4" />
+      <Flex justify="center" align="center" gap="4" py="4" wrap="wrap">
+        <Link href="https://saketlab.org" target="_blank" rel="noreferrer noopener" size="2" color="gray">
+          Saket Lab
+        </Link>
+        <Text size="2" color="gray">·</Text>
+        <Link href="mailto:saketc@iitb.ac.in" size="2" color="gray">
+          Contact
+        </Link>
+      </Flex>
+    </footer>
+  );
+}
