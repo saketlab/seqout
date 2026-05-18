8785d0b0f7532476f5d99f34838d76829aa478aa
Mukesh <hey.mukesh272921@gmail.com>
2026-05-18 16:53:58 +0530

    chore: refactor a bit

--- python/src/seqoutdb/__init__.py
+++ python/src/seqoutdb/__init__.py
@@ -1,5 +1,24 @@
 from seqoutdb.async_seqout import AsyncSeqout
-from seqoutdb.models import SearchParams
+from seqoutdb.models import (
+    NextCursor,
+    Publication,
+    SearchParams,
+    SearchResponse,
+    SearchResult,
+    SearchResults,
+    SearchStructuredParams,
+)
 from seqoutdb.seqout import Seqout
 
-__all__ = ["Seqout", "AsyncSeqout", "SearchParams"]
+# def search_structured(self, params: SearchStructuredParams) -> SearchResults:
+__all__ = [
+    "AsyncSeqout",
+    "NextCursor",
+    "Publication",
+    "SearchParams",
+    "SearchResponse",
+    "SearchResult",
+    "SearchResults",
+    "SearchStructuredParams",
+    "Seqout",
+]
