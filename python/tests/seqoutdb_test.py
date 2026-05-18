b732fcd4d68471317b1e00741b64b443820a1433
Mukesh <hey.mukesh272921@gmail.com>
2026-05-18 15:22:45 +0530

    feat: add more filtering methods

--- python/tests/seqoutdb_test.py
+++ python/tests/seqoutdb_test.py
@@ -1,5 +1,8 @@
 from seqoutdb import SearchParams, Seqout
 
 with Seqout() as sq:
-    results = sq.search(params=SearchParams(q="whole genome sequencing"))
-    print(len(results.by_organism("homo sapiens")))
+    results = sq.search(params=SearchParams(q="oxford nanopore"))
+    # get top 20 cited papers from china on "oxford nanopore" from geo
+    results.filter(country_code="chn", source="geo").top_cited(n=20).to_csv(
+        "./output/dump.csv"
+    )
